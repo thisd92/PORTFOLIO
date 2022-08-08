28ca033218d1ab6759f5532836def93856da6f92
thisd92 <thiiagodutra@gmail.com>
2022-08-08 17:04:10 -0300

    components

--- Vue2_/calculator/src/components/Button.vue
+++ Vue2_/calculator/src/components/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="button"
+    @click="$emit('onCalcClick', label)"
     :class="{ double, triple, operation }">
       {{ label }}
   </button>
@@ -18,6 +19,38 @@ export default {
 
 <style>
 :root{
-    
+    --bg-buton: #f0f0f0;
+    --border-button: solid 1 px #888;
 }
+
+.buton{
+  font-size: 1.4em;
+  background-color: var(--bg-buton);
+  border: none;
+  border-right: var(--border-button);
+  border-bottom: var(--border-button);
+  outline: none;
+}
+
+.button:active{
+  background-color: #ccc;
+}
+
+.button.double{
+  grid-column: span 2;
+}
+
+.button.triple{
+  grid-column: span 3;
+}
+
+.button.operation{
+  background-color: #fa8231;
+  color: #fff; 
+}
+
+.button.operation:active{
+  background-color: #fa8231cc;
+}
+
 </style>
--- Vue2_/calculator/src/main/Calculator.vue
+++ Vue2_/calculator/src/main/Calculator.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="calculator">
-      <Display></Display>
-      <Button></Button>
+      <Display value="0"/>
+      <Button label="AC" triple @onCalcClick="clearMemory" />
+      <Button label="/" operation @onCalcClick="setOperation" />
+      <Button label="7" @onCalcClick="addDigit" />
+      <Button label="8" @onCalcClick="addDigit" />
+      <Button label="9" @onCalcClick="addDigit" />
+      <Button label="*" operation @onCalcClick="setOperation" />
+      <Button label="4" @onCalcClick="addDigit" />
+      <Button label="5" @onCalcClick="addDigit" />
+      <Button label="6" @onCalcClick="addDigit" />
+      <Button label="-" operation @onCalcClick="setOperation" />
+      <Button label="1" @onCalcClick="addDigit" />
+      <Button label="2" @onCalcClick="addDigit" />
+      <Button label="3" @onCalcClick="addDigit" />
+      <Button label="+" operation @onCalcClick="setOperation" />
+      <Button label="0" double @onCalcClick="addDigit" />
+      <Button label="." @onCalcClick="addDigit" />
+      <Button label="=" operation @onCalcClick="setOperation" />
   </div>
 </template>
 
@@ -9,7 +25,18 @@
 import Display from "../components/Display"
 import Button from "../components/Button"
 export default {
-    components:{ Button, Display }
+    components:{ Button, Display },
+    methods:{
+        clearMemory(){
+            console.log('Limpar Memória')
+        },
+        setOperation(operation){
+            console.log('Operação ' + operation)
+        },
+        addDigit(n){
+            console.log('Digito ' + n)
+        }
+    }
 }
 </script>
 
