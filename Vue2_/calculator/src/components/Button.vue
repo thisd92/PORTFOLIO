0b3f6894ca88871590c5a4aa4cc64ea19ae403e1
thisd92 <80495413+thisd92@users.noreply.github.com>
2022-08-08 18:51:33 -0300

    components

--- Vue2_/calculator/src/components/Button.vue
+++ Vue2_/calculator/src/components/Button.vue
@@ -1,23 +1,57 @@
 <template>
-  <button class="button"
-    :class="{ double, triple, operation }">
-      {{ label }}
+  <button
+    class="button"
+    @click="$emit('onClicl', label)"
+    :class="{ double, triple, operation }"
+  >
+    {{ label }}
   </button>
 </template>
 
 <script>
 export default {
-    props: {
-        label: {},
-        operation: { type: Boolean },
-        double: { type: Boolean },
-        triple: { type: Boolean }
-    }
-}
+  props: {
+    label: {},
+    operation: { type: Boolean },
+    double: { type: Boolean },
+    triple: { type: Boolean },
+  },
+};
 </script>
 
 <style>
-:root{
-    
+:root {
+  --bg-buton: #f0f0f0;
+  --border-button: solid 1 px #888;
+}
+
+.buton {
+  font-size: 1.4em;
+  background-color: var(--bg-buton);
+  border: none;
+  border-right: var(--border-button);
+  border-bottom: var(--border-button);
+  outline: none;
+}
+
+.button:active {
+  background-color: #ccc;
+}
+
+.button.double {
+  grid-column: span 2;
+}
+
+.button.triple {
+  grid-column: span 3;
+}
+
+.button.operation {
+  background-color: #fa8231;
+  color: #fff;
+}
+
+.button.operation:active {
+  background-color: #fa8231cc;
 }
 </style>
--- Vue2_/calculator/src/main/Calculator.vue
+++ Vue2_/calculator/src/main/Calculator.vue
@@ -1,27 +1,57 @@
 <template>
   <div class="calculator">
-      <Display></Display>
-      <Button></Button>
+    <Display value="0" />
+    <Button label="AC" triple @onCalcClick="clearMemory" />
+    <Button label="/" operation @onCalcClick="setOperation" />
+    <Button label="7" @onCalcClick="addDigit" />
+    <Button label="8" @onCalcClick="addDigit" />
+    <Button label="9" @onCalcClick="addDigit" />
+    <Button label="*" operation @onCalcClick="setOperation" />
+    <Button label="4" @onCalcClick="addDigit" />
+    <Button label="5" @onCalcClick="addDigit" />
+    <Button label="6" @onCalcClick="addDigit" />
+    <Button label="-" operation @onCalcClick="setOperation" />
+    <Button label="1" @onCalcClick="addDigit" />
+    <Button label="2" @onCalcClick="addDigit" />
+    <Button label="3" @onCalcClick="addDigit" />
+    <Button label="+" operation @onCalcClick="setOperation" />
+    <Button label="0" double @onCalcClick="addDigit" />
+    <Button label="." @onCalcClick="addDigit" />
+    <Button label="=" operation @onCalcClick="setOperation" />
   </div>
 </template>
 
 <script>
-import Display from "../components/Display"
-import Button from "../components/Button"
+import Display from "../components/Display";
+import Button from "../components/Button";
 export default {
-    components:{ Button, Display }
-}
+  components: { Button, Display },
+  methods: {
+    clearMemory() {
+      // eslint-disable-next-line no-console
+      console.log("Limpar Memória");
+    },
+    setOperation(operation) {
+      // eslint-disable-next-line no-console
+      console.log("Operação " + operation);
+    },
+    addDigit(n) {
+      // eslint-disable-next-line no-console
+      console.log("Digito " + n);
+    },
+  },
+};
 </script>
 
 <style>
-.calculator{
-    height: 320px;
-    width: 235px;
-    border-radius: 5px;
-    overflow: hidden;
+.calculator {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
 
-    display: grid;
-    grid-template-columns: repeat(4, 25%);
-    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr 48px 48px 48px 48px 48px;
 }
 </style>
