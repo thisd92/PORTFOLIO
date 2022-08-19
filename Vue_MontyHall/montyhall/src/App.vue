84716a37d5131137183567d33e559d763a632afe
thisd92 <thiiagodutra@gmail.com>
2022-08-19 12:04:26 -0300

    project finished

--- Vue_MontyHall/montyhall/src/App.vue
+++ Vue_MontyHall/montyhall/src/App.vue
@@ -1,7 +1,25 @@
 <template>
     <div id="app">
         <h1>Problema de Monty Hall</h1>
-        <DoorObj number="1" :hasGift="false" />
+        <div class="form">
+            <div v-if="!started">
+                <label for="doorsAmount">Quantas portas?</label>
+                <input type="text" id="doorsAmount" size="3"
+                    v-model.number="doorsAmount">
+            </div>
+            <div v-if="!started">
+                <label for="selectedDoor">Qual a porta é a premiada?</label>
+                <input type="text" id="selectedDoor" size="3"
+                    v-model.number="selectedDoor">
+            </div>
+            <button v-if="!started" @click="started = true">Iniciar</button>
+            <button v-if="started" @click="started = false">Reiniciar</button>
+        </div>
+        <div class="doors" v-if="started">
+            <div v-for="i in doorsAmount" :key="i">
+            <DoorObj :hasGift="i === selectedDoor" :number="i" />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,7 +28,14 @@ import DoorObj from './components/DoorObj';
 
 export default {
     name: 'App',
-    components: { DoorObj }
+    components: { DoorObj },
+    data: function() {
+        return {
+            started: false,
+            doorsAmount: 3,
+            selectedDoor: null
+        }
+    }
 }
 
 </script>
@@ -37,5 +62,26 @@ body{
     background-color: #0004;
     padding: 20px;
     margin-bottom: 60px;
+}
+
+.form{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+}
+
+.form, .form input, .form button{
+    margin-bottom: 10px;
+    font-size: 2rem;
+}
+
+.doors{
+    align-self: stretch;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+
 }
 </style>
